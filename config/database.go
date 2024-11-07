f619ad044001afc628aa289a23e312e9112740c7
oadultradeepfield <phanuphat.srisukhawasu@gmail.com>
2024-11-07 14:09:15 +0800

    Fix prepare statements

--- config/database.go
+++ config/database.go
@@ -1,23 +1,32 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/oadultradeepfield/keepactive-backend/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func InitDB() *gorm.DB {
     dsn := os.Getenv("DATABASE_URL")
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+        Logger: logger.Default.LogMode(logger.Info),
+        PrepareStmt: true,
+    })
     if err != nil {
         log.Fatal("Failed to connect to database:", err)
     }
 
     // Auto migrate schemas
     db.AutoMigrate(&models.User{}, &models.Website{})
+    session := db.Session(&gorm.Session{PrepareStmt: true})
+    if session != nil {
+        fmt.Println("Migration successful")
+    }
 
     return db
 }
